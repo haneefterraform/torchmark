86f791f5339104a71081dd4cf2160f453bc3791c
haneefterraform <162335794+haneefterraform@users.noreply.github.com>
2024-06-23 16:01:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,83 +1,83 @@
+
+provider "aws" {
+  region = var.region
+
+}
 resource "aws_instance" "windows" {
-    ami = data.aws_ami.windows.id
-    instance_type = var.instance_type
-    key_name = var.keyname
-    subnet_id =     var.subnet
-    vpc_security_group_ids = var.securitygroup
-    tags = local.common_tags_dev
+  ami                    = data.aws_ami.windows.id
+  instance_type          = var.instance_type
+  key_name               = var.keyname
+  subnet_id              = var.subnet
+  vpc_security_group_ids = var.securitygroup
+  #tags                   = local.common_tags_dev
+  tags = var.env == "dev" ? local.common_tags_dev : local.common_tags_prd
 
-
-    root_block_device {
-    volume_type = "gp3"
-    iops        = 3000
-    throughput  = 125
-    volume_size = 30
+  root_block_device {
+    volume_type           = "gp3"
+    iops                  = 3000
+    throughput            = 125
+    volume_size           = var.root_voulme_size
     delete_on_termination = true
-    tags = local.common_tags_dev
-}
-}
-
-resource "aws_ebs_volume" "datadrive1" {
-  availability_zone = "us-east-1a"
-  size = var.size
-  type = "gp3"
-  tags = local.common_tags_dev
-
+    tags = var.env == "dev" ? local.common_tags_dev : local.common_tags_prd
+  }
 }
 
-resource "aws_volume_attachment" "generic_data_vol_att0" {
-  device_name = "/dev/xvdf"
-  volume_id   = aws_ebs_volume.datadrive1.id
+resource "aws_ebs_volume" "datadrives" {
+  count             = length(var.data_drives)
+  availability_zone = aws_instance.windows.availability_zone
+  size              = var.data_drives[count.index].volume_size
+  type              = var.data_drives[count.index].volume_type
+  encrypted         = true
+
+  tags = merge(
+     var.env == "dev" ? local.common_tags_dev: local.common_tags_prd,
+    
+
+    {
+
+     Name = "servername_datadisk${count.index}"
+      
+   
+    Label =  aws_ebs_volume.datadrives[0] == "servername_datadisk1" ? "temp" : "kjk"
+       depends_on = [
+      aws_ebs_volume.datadrives.count
+  ]
+}
+     
+
+  )
+
+}
+    
+  
+
+resource "aws_volume_attachment" "multiple_drives" {
+  count       = length(var.data_drives)
+  device_name = var.data_drives[count.index].device_name
+  volume_id   = aws_ebs_volume.datadrives[count.index].id
   instance_id = aws_instance.windows.id
-  
-
 }
 
-resource "aws_ebs_volume" "datadrive2" {
-  availability_zone = "us-east-1a"
-  size = 40
-  type = "gp3"
-  tags = local.common_tags_dev
 
-}
-
-resource "aws_volume_attachment" "generic_data_vol_att1" {
-  device_name = "/dev/xvdg"
-  volume_id   = aws_ebs_volume.datadrive2.id
-  instance_id = aws_instance.windows.id
-  
-
-}
-locals {
-    common_tags_prd = {
-        Name = "servername"
-        os   = "windows"
-        awsbackup = "Prod"
-    }
-  
-}
 
 locals {
-    common_tags_dev = {
-        Name = "servername"
-        os   = "windows"
-        awsbackup = "Dev"
-        testing = "target"
-    }
-  
+  common_tags_prd = {
+    Name      = "servername"
+    os        = "windows"
+    awsbackup = "Prod"
+    Ritm      = var.request
+  }
+
 }
 
-output "public_ip" {
-    value = aws_instance.windows.public_ip
-  
+locals {
+  common_tags_dev = {
+    Name      = "servername"
+    os        = "windows"
+    awsbackup = "Dev"
+    Ritm      = var.request
+  }
 }
 
-output "datavolumeid0" {
-    value = aws_ebs_volume.datadrive1.id
-  
-}
 
-output "datavolumeid1" {
-    value = aws_ebs_volume.datadrive2.id
-  
-}
+
