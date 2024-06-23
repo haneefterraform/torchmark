a53921b806316ad8b8b3283e07382a9ef595ee81
haneefterraform <162335794+haneefterraform@users.noreply.github.com>
2024-06-23 16:02:11 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,1 +1,15 @@
+output "private_ip" {
+  value = aws_instance.windows.private_ip
 
+}
+
+output "datadrives" {
+  value = aws_ebs_volume.datadrives.*.id
+
+}
+
+
+output "SG" {
+  value = aws_instance.windows.*.security_groups 
+  
+}
